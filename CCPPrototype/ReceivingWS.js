e44745407b24d8e169ff8052326a20f4bd92995a
Qisen25 <Qisen25@github.com>
2020-07-21 22:47:15 +0800

    Played around more with ws

--- CCPPrototype/ReceivingWS.js
+++ CCPPrototype/ReceivingWS.js
@@ -1,9 +1,22 @@
+const https = require('https');
 const http = require('http');
+const fs = require('fs');
 const WebSocket = require('ws');
 const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 3000);
-const server = http.createServer(app);
+
+//https cert and keys
+const options = {
+    key: fs.readFileSync('server-key.pem'), 
+    cert: fs.readFileSync('server-crt.pem'), 
+    ca: fs.readFileSync('ca-crt.pem'), 
+};
+
+
+const insecServer = http.createServer(app);
+
+const server = https.createServer(options, app);
 
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -13,22 +26,28 @@ const rl = readline.createInterface({
 
 // const server = http.createServer();
 
-const wsServer = new WebSocket.Server({port: 3012});
+const wsServer = new WebSocket.Server({server});
 
 app.get('/', (req, res) => {
     //res.send('Server is running on port 3000');
     res.sendFile('MapPageWS.html', { root: __dirname});
 });
 
-
+//when client establishes client to server
 wsServer.on('connection', function connection(ws, request, client) {
-	ws.on('message', function incoming(message) {
-		ws.id = JSON.parse(message).id;
-		console.log(ws.id + ' has joined');
-	});
+	// ws.on('message', function incoming(message) {
+	// 	ws.id = JSON.parse(message).id;
+	// 	console.log(ws.id + ' has joined');
+	// });
+    ws.id = request.socket.remoteAddress;
+    console.log(ws.id + ' has joined');
+
+    ws.on('close', function close() {//when client socket closes
+      console.log(ws.id + ' has disconnected');
+    });
 });
 
-console.log('ReceivingWS listening on PORT 3000');
+console.log('ReceivingWS listening on PORT 3001');
 
 // rl.question("Enter to view list of clients", function(keyP) {
 // 	if(keyP === '\r'){
@@ -38,7 +57,10 @@ console.log('ReceivingWS listening on PORT 3000');
 // 	}
 // });
 
-server.listen(app.get("port"));
+//http
+insecServer.listen(app.get("port"));
+//https
+server.listen(3001);
 
 process.stdin.on('keypress', (str, key) => {
   // console.log(str)
